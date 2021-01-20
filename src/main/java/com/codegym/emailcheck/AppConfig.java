b1f6b3f209d258995b7fa65d04599026937fdd59
= <=>
2021-01-20 11:49:23 +0700

    abc

---
+++ src/main/java/com/codegym/emailcheck/AppConfig.java
@@ -0,0 +1,35 @@
+package com.codegym.emailcheck;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.codegym.emailcheck")
+public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
